8443d032deba1a5a75aebad0b67743979e1e29ac
unoffending <109032978+unoffending@users.noreply.github.com>
2023-05-20 17:20:09 +0600

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,35 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if(symbol == 'q' || symbol == 'w' || 
+           symbol == 'e' || symbol == 'r' || 
+           symbol == 't' || symbol == 'y' || 
+           symbol == 'u' || symbol == 'i' || 
+           symbol == 'o' || symbol == 'p' || 
+           symbol == 'a' || symbol == 's' || 
+           symbol == 'd' || symbol == 'f' ||
+           symbol == 'g' || symbol == 'h' || 
+           symbol == 'j' || symbol == 'k' || 
+           symbol == 'l' || symbol == 'z' || 
+           symbol == 'x' || symbol == 'c' || 
+           symbol == 'v' || symbol == 'b' || 
+           symbol == 'n' || symbol == 'm' ||
+           symbol == 'Q' || symbol == 'W' || 
+           symbol == 'E' || symbol == 'R' || 
+           symbol == 'T' || symbol == 'Y' || 
+           symbol == 'U' || symbol == 'I' || 
+           symbol == 'O' || symbol == 'P' || 
+           symbol == 'A' || symbol == 'S' || 
+           symbol == 'D' || symbol == 'F' ||
+           symbol == 'G' || symbol == 'H' || 
+           symbol == 'J' || symbol == 'K' || 
+           symbol == 'L' || symbol == 'Z' || 
+           symbol == 'X' || symbol == 'C' || 
+           symbol == 'V' || symbol == 'B' || 
+           symbol == 'N' || symbol == 'M') {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
